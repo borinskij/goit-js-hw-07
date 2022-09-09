7b21d4d7188082bab5b28465c95c5f25f84c8d19
Abdrii Borinskij <aborinskij@gmail.com>
2022-09-09 20:06:55 +0300

    2 задача

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
 
 const difGaleryRef = document.querySelector('.gallery');
-// console.log(difGaleryRef)
 const markup = creatImgCardMarkup(galleryItems);
 
 difGaleryRef.insertAdjacentHTML('beforeend', markup)
-
-
-// console.log(creatImgCardMarkup(galleryItems));
 
 function creatImgCardMarkup(galleryItems) {
     return galleryItems.map(({ original, preview, description }) => {
@@ -33,13 +28,9 @@ difGaleryRef.addEventListener('click', onSrcModals);
 function onSrcModals(event) {
     event.preventDefault();
     const src = event.target.dataset.source
-    console.log(src)
- const instance = basicLightbox.create(`
+     const instance = basicLightbox.create(`
      <img src="${src}" width="800" height="600">
  `)
  
  instance.show()
 }
-console.log()
-//Подключение библиотеки
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryRef = document.querySelector('.gallery');
+const markup = creatImgCardMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', markup)
+
+function creatImgCardMarkup(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `
+                <a class="gallery__item"
+                href="${original}">
+                <img class="gallery__image"
+                src="${preview}" 
+                alt="${description}"/></a>
+                
+        `
+    }).join('');
+}
+
+galleryRef.addEventListener('click', onImgModal);
+
+function onImgModal(event) {
+    event.preventDefault();
+    const hrefModal = event.target.parentElement.href;
+    const lightbox = new SimpleLightbox('.gallery a', {hrefModal});
+
+}
+
+
+
+
+
+
+
